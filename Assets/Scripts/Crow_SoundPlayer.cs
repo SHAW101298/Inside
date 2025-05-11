51b86f29a800f04da8e3c5cc951c98eb0eb59fda
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-11 19:05:29 +0200

    Added Crow Model

---
+++ Assets/Scripts/Crow_SoundPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crow_SoundPlayer : MonoBehaviour
+{
+    public AudioSource source;
+
+    float timer;
+    public float minTimeDelay;
+    public float maxTimeDelay;
+    float timeDelay;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeDelay)
+        {
+            source.Play();
+            
+            timer = 0;
+            timeDelay = Random.Range(minTimeDelay, maxTimeDelay);
+        }
+    }
+}
