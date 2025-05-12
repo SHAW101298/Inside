65a11c2cd4979fcd996cfe9b8d291b149a5f8a5c
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-12 14:19:37 +0200

    Changed One Note Sound

---
+++ Assets/Scripts/MainMenu/UI_MainMenuControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MainMenuControl : MonoBehaviour
+{
+    public void BTN_Play()
+    {
+
+    }
+    public void BTN_HowToPlay()
+    {
+
+    }
+    public void BTN_Options()
+    {
+
+    }
+    public void BTN_Exit()
+    {
+
+    }
+}
--- Assets/Scripts/MainMenu/UI_MenuButton.cs
+++ Assets/Scripts/MainMenu/UI_MenuButton.cs
@@ -7,9 +7,15 @@ public class UI_MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 {
     [SerializeField] float scaleModifier;
     [SerializeField] RectTransform rect;
+    [SerializeField] AudioSource source;
     public void OnPointerEnter(PointerEventData eventData)
     {
         rect.localScale = Vector3.one * scaleModifier;
+        float randPitch = Random.Range(-0.15f, 0.15f);
+        float randSpan = Random.Range(-0.4f, 0.4f);
+        source.pitch = 1 + randPitch;
+        source.panStereo = randSpan;
+        source.Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
