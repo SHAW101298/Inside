7a82d131f7b832085bc2ed180fd12c2fa719dec3
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-23 00:03:20 +0200

    Modified Whisper Sources

--- Assets/Scripts/01/DangerZone.cs
+++ Assets/Scripts/01/DangerZone.cs
@@ -10,6 +10,12 @@ public class DangerZone : MonoBehaviour
     [SerializeField] Transform endPosition;
     [SerializeField] bool isActive;
     [SerializeField] List<AudioSource> whisperSources;
+    [SerializeField] float maxTimeDifference;
+    [SerializeField] float minTimeDifference;
+    [SerializeField] float timeDifference;
+    [SerializeField] float timer;
+    [SerializeField] bool startTimer;
+    [SerializeField] int lastIndex;
 
     private void Update()
     {
@@ -20,13 +26,51 @@ public class DangerZone : MonoBehaviour
                 source.volume = flame.dangerLevel;
             }
         }
+        SetRandomStartTimers();
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            flame.SetAsCurrentDanger(startPosition.position,endPosition.position);
+            ActivateDangerZone();
+        }
+    }
+    public void ActivateDangerZone()
+    {
+        flame.SetAsCurrentDanger(startPosition.position, endPosition.position);
+        isActive = true;
+        startTimer = true;
+        timer = 0;
+        timeDifference = Random.Range(minTimeDifference, maxTimeDifference);
+        lastIndex = 0;
+    }
+    public void DisableDangerZone()
+    {
+        isActive = false;
+        startTimer = false;
+        timer = 0;
+        lastIndex = 0;
+    }
+
+    void SetRandomStartTimers()
+    {
+        if (startTimer == true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= timeDifference)
+            {
+                timer -= timeDifference;
+                timeDifference = Random.Range(minTimeDifference, maxTimeDifference);
+                whisperSources[lastIndex].gameObject.SetActive(true);
+                lastIndex++;
+                if(lastIndex >= whisperSources.Count)
+                {
+                    startTimer = false;
+                    timer = 0;
+                }
+            }
         }
     }
 }
