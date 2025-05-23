7718360b08480851a14030b0716248d047288047
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-23 13:28:53 +0200

    Added ScreenShake Effect on Danger Zones

---
+++ Assets/Scripts/01/ScreenShakeEffect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeEffect : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] FlameVisibility flame;
+    [SerializeField] Transform target;
+    [Header("Values")]
+    [SerializeField] float maxScreenMovement;
+    float desiredScreenMovement;
+    [SerializeField] bool isShaking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isShaking == true)
+        {
+            desiredScreenMovement = flame.dangerLevel * maxScreenMovement;
+            Vector3 rand = new Vector3();
+            rand.x = Random.Range(-desiredScreenMovement, desiredScreenMovement);
+            rand.y = Random.Range(-desiredScreenMovement, desiredScreenMovement);
+            rand.z = Random.Range(-desiredScreenMovement, desiredScreenMovement);
+
+            target.transform.localEulerAngles = rand;
+        }
+    }
+}
