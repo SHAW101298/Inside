d0a13d03160f93aef5d476000625fdbb339eeab6
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-23 14:43:26 +0200

    Added words popping up in danger zone

---
+++ Assets/Scripts/01/ScreenWord.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScreenWord : MonoBehaviour
+{
+    [SerializeField] TMP_Text textField;
+    [SerializeField] float timeToLive;
+    [SerializeField] float lifeTimer;
+    [SerializeField] bool wordIsExpired;
+    [Header("Word Movement")]
+    [SerializeField] float erraticMovementAmount;
+    [SerializeField] float normalMovemntValue;
+    [Header("Transparency Control")]
+    [SerializeField] float transparencyTimer;
+    [SerializeField] float transparencyCeilingTime;
+    [SerializeField] bool reverse;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        transparencyCeilingTime = timeToLive / 2;
+    }
+    private void Update()
+    {
+        WordLife();
+        ErraticMovement();
+        WordTransparency();
+    }
+
+    public void WordLife()
+    {
+        lifeTimer += Time.deltaTime;
+
+        if (lifeTimer >= timeToLive)
+        {
+            wordIsExpired = true;
+        }
+    }
+    public void SetText(string tekst)
+    {
+        textField.text = tekst;
+    }
+    public bool CheckIfExpired()
+    {
+        return wordIsExpired;
+    }
+    public void ErraticMovement()
+    {
+        Vector3 mov = new Vector3();
+        mov.x = Random.Range(-erraticMovementAmount, erraticMovementAmount);
+        mov.y = Random.Range(-erraticMovementAmount, erraticMovementAmount);
+        gameObject.transform.localPosition += mov;
+    }
+    void WordTransparency()
+    {
+        if(reverse == false)
+        {
+            transparencyTimer += Time.deltaTime;
+            if(transparencyTimer >= transparencyCeilingTime)
+            {
+                transparencyTimer = transparencyCeilingTime;
+                reverse = true;
+            }
+        }
+        else
+        {
+            transparencyTimer -= Time.deltaTime;
+        }
+        textField.alpha = transparencyTimer / transparencyCeilingTime;
+    }
+}
---
+++ Assets/Scripts/01/ScreenWordsManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWordsManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] List<ScreenWord> currentWords;
+    [SerializeField] GameObject wordsContent;
+    [SerializeField] ScreenWord prefab;
+    [SerializeField] FlameVisibility flame;
+    [Header("Data")]
+    [SerializeField] int desiredNumberOfWords;
+    [SerializeField] float scaleVariation;
+    [SerializeField] float startPositionVariation;
+    [Header("Indexes of words")]
+    [SerializeField] int indexMin;
+    [SerializeField] int indexMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateDesiredNumberOfWords();
+        CheckIfNeedToAddMoreWords();
+        DeleteExpiredWords();
+
+    }
+
+    void CalculateDesiredNumberOfWords()
+    {
+        desiredNumberOfWords = (int)(flame.dangerLevel * 4);
+    }
+    void CheckIfNeedToAddMoreWords()
+    {
+        if(currentWords.Count < desiredNumberOfWords)
+        {
+            GameObject temp = Instantiate(prefab.gameObject);
+            temp.transform.SetParent(wordsContent.transform);
+            ScreenWord tempWord = temp.GetComponent<ScreenWord>();
+            currentWords.Add(tempWord);
+
+            // Set Random Text
+            int randIndex = Random.Range(indexMin, indexMax);
+            tempWord.SetText(LanguageManager.Instance.GetText(randIndex));
+            // Set Random Scale
+            float randScale = Random.Range(-scaleVariation, scaleVariation);
+            Vector3 newScale = Vector3.one * randScale;
+            temp.transform.localScale = Vector3.one + newScale;
+            // Set Random Position
+            float randPosition = Random.Range(-startPositionVariation, startPositionVariation);
+            Vector3 newPosition = new Vector3();
+            newPosition.x = randPosition;
+            randPosition = Random.Range(-startPositionVariation, startPositionVariation);
+            newPosition.y = randPosition;
+            newPosition.z = 0;
+            temp.transform.localPosition = newPosition;
+
+        }
+    }
+    void DeleteExpiredWords()
+    {
+        if (currentWords.Count <= 0)
+            return;
+
+        for(int i = 0; i >= 0; i--)
+        {
+            if (currentWords[i].CheckIfExpired() == true)
+            {
+                Destroy(currentWords[i].gameObject);
+                currentWords.RemoveAt(i);
+            }
+        }
+    }
+
+
+
+}
