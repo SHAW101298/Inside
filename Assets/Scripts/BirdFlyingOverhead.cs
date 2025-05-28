05aa69bbff3baea3d7e830c319cccdd9ac7ffe02
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-28 22:34:57 +0200

    Added Flying overhead crows

---
+++ Assets/Scripts/BirdFlyingOverhead.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEditor.PlayerSettings;
+
+public class BirdFlyingOverhead : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] FlyingBirdsController controller;
+    [SerializeField] Animator anim;
+    [SerializeField] Transform startPosition;
+    [SerializeField] List<Transform> possibleDestinations;
+
+    [Header("Data")]
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Vector3 destination;
+
+    public UnityEvent EVENT_ReachedDestination;
+    float dist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * direction;
+        dist = Vector3.Distance(transform.position, destination);
+
+        if(dist <= 2f)
+        {
+            EVENT_ReachedDestination.Invoke();
+            controller.MarkMeAsInactive(this);
+        }
+        
+    }
+    public void GetNewDestination()
+    {
+        int rand = Random.Range(0, possibleDestinations.Count);
+        destination = possibleDestinations[rand].position;
+        transform.position = startPosition.position;
+        direction = (destination - transform.position).normalized;
+        transform.LookAt(destination);
+        anim.SetBool("Flying", true);
+    }
+    public void TriggerDestruction()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/FlyingBirdsController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBirdsController : MonoBehaviour
+{
+    [SerializeField] List<BirdFlyingOverhead> activeBirds;
+    [SerializeField] List<BirdFlyingOverhead> inactiveBirds;
+    [SerializeField] int desiredAmountOfActiveBirds;
+    [SerializeField] int amountOfActiveBirds;
+
+    private void Start()
+    {
+        int rand;
+        for(int i = 0; i < desiredAmountOfActiveBirds; i++)
+        {
+            ActivateRandomBird();
+        }
+        foreach(BirdFlyingOverhead bird in activeBirds)
+        {
+            bird.gameObject.SetActive(true);
+            bird.GetNewDestination();
+        }
+    }
+
+    public void MarkMeAsInactive(BirdFlyingOverhead bird)
+    {
+        activeBirds.Remove(bird);
+        inactiveBirds.Add(bird);
+        bird.gameObject.SetActive(false);
+        amountOfActiveBirds--;
+
+        if(amountOfActiveBirds < desiredAmountOfActiveBirds)
+        {
+            ActivateRandomBird();
+        }
+    }
+    public void ActivateRandomBird()
+    {
+        int rand = Random.Range(0, inactiveBirds.Count);
+        Debug.Log("rand = " + rand);
+        BirdFlyingOverhead bird = inactiveBirds[rand];
+        Debug.Log("bird = " + bird.gameObject.name);
+
+        activeBirds.Add(bird);
+        inactiveBirds.Remove(bird);
+
+        bird.gameObject.SetActive(true);
+        bird.GetNewDestination();
+        amountOfActiveBirds++;
+
+    }
+
+}
