5887e32eb2c6c289c37d102b79aa6eedb4dfa32a
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-05-28 15:07:36 +0200

    Added dim zones to lore, temple and road

--- Assets/Scripts/LevelScript_02.cs
+++ Assets/Scripts/LevelScript_02.cs
@@ -19,16 +19,11 @@ public class LevelScript_02 : MonoBehaviour
     [SerializeField] List<GameObject> objectsAfterPlacingPiece;
 
     [Header("Checkers")]
-    [SerializeField] float fogAmountOnRoad;
-    [SerializeField] float fogAmountNormal;
     [SerializeField] Animator doorAnimator;
 
     public bool foundMissingPiece;
     public bool placedMissingPiece;
-    public bool roadTimerFade;
-    public bool roadTimerShine;
-    public float roadTimer;
-    public float roadTimerAmount;
+
 
     public void FoundMissingPiece()
     {
@@ -36,10 +31,6 @@ public class LevelScript_02 : MonoBehaviour
         foundMissingPiece = true;
         emptySpotReadyForPiece.SetActive(true);
         emptySpot.SetActive(false);
-        RenderSettings.fogEndDistance = fogAmountNormal;
-
-        roadCrowsBefore.SetActive(false);
-        roadCrowsAfter.SetActive(true);
     }
 
     public void PlacedMissingPiece()
@@ -63,53 +54,10 @@ public class LevelScript_02 : MonoBehaviour
         doorAnimator.SetTrigger("Open");
     }
 
-    public void EnterTheRoad()
-    {
-        Debug.Log("Enter The road");
-        roadTimerFade = true;
-        roadTimerShine = false;
-    }
-    public void ExitTheRoad()
-    {
-        Debug.Log("Exit The road");
-        roadTimerShine = true;
-        roadTimerFade = false;
-    }
+
 
     private void Update()
     {
-        if(roadTimerFade == true)
-        {
-            roadTimer += Time.deltaTime;
-            float percent = roadTimer / roadTimerAmount;
-            float fogDist = Mathf.Lerp(fogAmountNormal, fogAmountOnRoad, percent);
-            RenderSettings.fogEndDistance = fogDist;
-            musicAudioSource.volume = Mathf.Lerp(0.5f, 0, percent);
 
-            if(roadTimer >= roadTimerAmount)
-            {
-                roadTimerFade = false;
-                roadTimer = roadTimerAmount;
-                RenderSettings.fogEndDistance = fogAmountOnRoad;
-                musicAudioSource.volume = 0;
-            }
-        }
-
-        if(roadTimerShine == true)
-        {
-            roadTimer -= Time.deltaTime;
-            float percent = roadTimer / roadTimerAmount;
-            float fogDist = Mathf.Lerp(fogAmountNormal, fogAmountOnRoad, percent);
-            RenderSettings.fogEndDistance = fogDist;
-            musicAudioSource.volume = Mathf.Lerp(0.5f, 0, percent);
-
-            if (roadTimer <= 0)
-            {
-                roadTimerShine = false;
-                roadTimer = 0;
-                RenderSettings.fogEndDistance = fogAmountNormal;
-                musicAudioSource.volume = 0.5f;
-            }
-        }
     }
 }
