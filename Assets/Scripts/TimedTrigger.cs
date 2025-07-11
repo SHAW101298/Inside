645479cc9106fbe34325dbcfa1a3f0a7e9d4051e
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-07-11 10:49:10 +0200

    Added Timed Trigger

---
+++ Assets/Scripts/TimedTrigger.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimedTrigger : MonoBehaviour
+{
+    public UnityEvent interactEvent;
+    [Space(10)]
+    [SerializeField] bool runTriggerOnStart;
+    [SerializeField] bool destroyTriggerOnActivation;
+    [SerializeField] bool disableTriggerOnActivation;
+    [Space(10)]
+    [SerializeField] float timeBetweenActivations;
+    [SerializeField] float timer;
+    [Space(10)]
+    [SerializeField] List<GameObject> objectsGroup1;
+    [SerializeField] List<GameObject> objectsGroup2;
+
+    [SerializeField] bool debugTriggerInteraction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (runTriggerOnStart == true)
+        {
+            TriggerInteraction();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugTriggerInteraction == true)
+        {
+            Debug.Log("Interaction Triggered be Debug");
+            debugTriggerInteraction = false;
+            TriggerInteraction();
+        }
+
+        timer += Time.deltaTime;
+        if(timer >= timeBetweenActivations)
+        {
+            TriggerInteraction();
+            timer = 0;
+        }
+    }
+
+    public void TriggerInteraction()
+    {
+        Interact();
+    }
+    public void TriggerDestruction()
+    {
+        Destroy(gameObject);
+    }
+    public void StartTimer()
+    {
+        gameObject.SetActive(true);
+    }
+    void Interact()
+    {
+        interactEvent.Invoke();
+
+        if (destroyTriggerOnActivation == true)
+        {
+            Destroy(gameObject);
+        }
+        if (disableTriggerOnActivation == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    public void EnableGroup1()
+    {
+        if (objectsGroup1.Count > 0)
+        {
+            foreach (GameObject obj in objectsGroup1)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+    public void EnableGroup2()
+    {
+        if (objectsGroup2.Count > 0)
+        {
+            foreach (GameObject obj in objectsGroup2)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+    public void DisableGroup1()
+    {
+        if (objectsGroup1.Count > 0)
+        {
+            foreach (GameObject obj in objectsGroup1)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+    public void DisableGroup2()
+    {
+        if (objectsGroup2.Count > 0)
+        {
+            foreach (GameObject obj in objectsGroup2)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+    public void DestroyGroup1()
+    {
+        foreach (GameObject obj in objectsGroup1)
+        {
+            Destroy(obj);
+        }
+        objectsGroup1.Clear();
+    }
+    public void DestroyGroup2()
+    {
+        foreach (GameObject obj in objectsGroup2)
+        {
+            Destroy(obj);
+        }
+        objectsGroup2.Clear();
+    }
+    public void TeleportGroup1ToGroup2()
+    {
+        foreach (GameObject obj in objectsGroup1)
+        {
+            obj.transform.position = objectsGroup2[0].transform.position;
+        }
+    }
+    public void EnableRandomObjectInGroup1()
+    {
+        int rand = Random.Range(0, objectsGroup1.Count);
+        objectsGroup1[rand].SetActive(true);
+    }
+    public void EnableRandomObjectInGroup2()
+    {
+        int rand = Random.Range(0, objectsGroup2.Count);
+        objectsGroup2[rand].SetActive(true);
+    }
+    public void CheckValidityOfLists()
+    {
+        for (int i = objectsGroup1.Count - 1; i >= 0; i--)
+        {
+            if (objectsGroup1[i] == null)
+            {
+                objectsGroup1.RemoveAt(i);
+            }
+        }
+        for (int i = objectsGroup2.Count - 1; i >= 0; i--)
+        {
+            if (objectsGroup2[i] == null)
+            {
+                objectsGroup2.RemoveAt(i);
+            }
+        }
+    }
+    public void InteractWithRandomTriggerInGroup1()
+    {
+        int rand = Random.Range(0, objectsGroup1.Count);
+        objectsGroup1[rand].GetComponentInChildren<InteractTrigger>().TriggerInteraction();
+    }
+
+}
