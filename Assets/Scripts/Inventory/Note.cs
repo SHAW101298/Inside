0f07d8da368a2b1e23cbb0fffec816c3f218910c
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-11-26 18:28:33 +0100

    More Work on Notes. Issue found
    Create Prefabs of notes on each scene. The data wont survive between scene loads

--- Assets/Scripts/Inventory/Note.cs
+++ Assets/Scripts/Inventory/Note.cs
@@ -7,6 +7,14 @@ public class Note : MonoBehaviour
 {
     [SerializeField] List<TMP_Text> textFields;
     List<LanguageApplier> lang;
+    [SerializeField] bool setIndexOnStart;
+    [SerializeField] int startIndex;
+
+    private void Start()
+    {
+        GetLangAppliers();
+        SetIndexes();
+    }
 
     public void ChangeLangData(int line, int newIndex)
     {
@@ -15,6 +23,25 @@ public class Note : MonoBehaviour
     public int GetLangIndex(int line)
     {
         return lang[line].GetIndex();
+    }
+    public void SetIndexes()
+    {
+        if (setIndexOnStart == false)
+            return;
+        int index = startIndex;
+        for(int i = 0; i < lang.Count; i++)
+        {
+            lang[i].ChangeIndexTo(index);
+            index++;
+        }
+    }
+    void GetLangAppliers()
+    {
+        lang = new List<LanguageApplier>();
+        for(int i = 0; i< textFields.Count; i++)
+        {
+            lang.Add(textFields[i].GetComponent<LanguageApplier>());
+        }
     }
 
 }
