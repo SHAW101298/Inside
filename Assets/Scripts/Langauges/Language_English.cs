da17898fe58ac57d12ef51e975a9f6b58e3c3074
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2026-01-14 19:31:40 +0100

    Placed Dialog Person in middle of island 4

--- Assets/Scripts/Langauges/Language_English.cs
+++ Assets/Scripts/Langauges/Language_English.cs
@@ -574,6 +574,8 @@ public class Language_English : LanguageBase
         loadedText[90] = "Are you lost ?";
         loadedText[91] = "Or maybe you lost something ?";
         loadedText[92] = "Or maybe its the other way around ?";
+        loadedText[93] = "All these signs, pointing in every direction.";
+        loadedText[94] = "And yet, no step taken on any path. The irony of it";
 
 
         for (int i = 0; i < 200; i++)
