11912afd73379a03c1adafe43bc06e5f9961a47c
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2026-01-15 19:20:19 +0100

    Added few icons

---
+++ Assets/Graphics/03/AttributionsToArt.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AttributionsToArt : MonoBehaviour
+{
+    // <a href="https://www.flaticon.com/free-icons/ribbon" title="ribbon icons">Ribbon icons created by Victoruler - Flaticon</a>
+    // <a href = "https://www.flaticon.com/free-icons/duration" title="duration icons">Duration icons created by Victoruler - Flaticon</a>
+    // <a href="https://www.flaticon.com/free-icons/hammer" title="hammer icons">Hammer icons created by Icon Mart - Flaticon</a>
+    // <a href="https://www.flaticon.com/free-icons/snowglobe" title="snowglobe icons">Snowglobe icons created by apien - Flaticon</a>
+    // <a href="https://www.flaticon.com/free-icons/dagger" title="dagger icons">Dagger icons created by berkahicon - Flaticon</a>
+
+}
