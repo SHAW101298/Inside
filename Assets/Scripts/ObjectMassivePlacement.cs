02a00be0a645460e7d8442da7c57150db64e74a7
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2026-01-25 17:26:30 +0100

    Placed Grass on island 5

--- Assets/Scripts/ObjectMassivePlacement.cs
+++ Assets/Scripts/ObjectMassivePlacement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -7,6 +8,7 @@ public class ObjectMassivePlacement : MonoBehaviour
 {
     [SerializeField] GameObject objectToSpawn;
     [SerializeField] Transform middleOfCircle;
+    [SerializeField] Transform originOfRay;
     [SerializeField] float circlePlacementSize;
     [SerializeField] int amountOfObjectsToPlace;
     [SerializeField] LayerMask raycastLayer;
@@ -28,24 +30,32 @@ public class ObjectMassivePlacement : MonoBehaviour
         }
         runScript = false;
         RaycastHit hit;
-        Vector3 raycastPoint = middleOfCircle.transform.position + new Vector3(0, 50, 0);
-        Vector3 dir = middleOfCircle.position - raycastPoint;
+        Vector3 dir;
         Vector3 randRot;
         Vector3 randScale;
+        Vector3 randSpot;
         GameObject temp;
         for(int i = 0; i < amountOfObjectsToPlace; i++)
         {
-            if(Physics.Raycast(raycastPoint, dir, out hit, 500f, raycastLayer) == true)
+            Vector2 randomCircle = Random.insideUnitCircle * circlePlacementSize;
+            Debug.Log("random circle = " + randomCircle);
+            randSpot = new Vector3(randomCircle.x, 0, randomCircle.y) + middleOfCircle.position ;
+            dir = (randSpot - originOfRay.position).normalized;
+
+
+
+            if(Physics.Raycast(originOfRay.position, dir, out hit, 500f, raycastLayer) == true)
             {
+                Debug.Log("hit point is = " + hit.point);
                 temp = Instantiate(objectToSpawn);
                 temp.transform.position = hit.point;
                 // transform randomization
                 randRot.x = Random.Range(-rotationRandomization.x, rotationRandomization.x);
                 randRot.y = Random.Range(-rotationRandomization.y, rotationRandomization.y);
                 randRot.z = Random.Range(-rotationRandomization.z, rotationRandomization.z);
-                randScale.x = Random.Range(-scaleRandomization.x, scaleRandomization.x);
-                randScale.y = Random.Range(-scaleRandomization.y, scaleRandomization.y);
-                randScale.z = Random.Range(-scaleRandomization.z, scaleRandomization.z);
+                randScale.x = Random.Range(1 - scaleRandomization.x, 1 + scaleRandomization.x);
+                randScale.y = Random.Range(1 - scaleRandomization.y, 1 + scaleRandomization.y);
+                randScale.z = Random.Range(1 - scaleRandomization.z, 1 + scaleRandomization.z);
                 temp.transform.localEulerAngles = randRot;
                 temp.transform.localScale = randScale;
             }
